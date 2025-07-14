eec7652e6353a81c7a251a7b04273e554bf5a8d0
Eric <e1977710@gmail.com>
2025-07-14 14:56:07 +0100

    homeworkgit add .

---
+++ homework/c++.cpp
@@ -0,0 +1,146 @@
+
+#include <iostream>
+#include <string>
+/*
+int foo(int a, int b, int c) {
+    int min = a;
+
+    if (b < min) {
+        min = b;
+    }
+
+    if (c < min) {
+        min = c;
+    }
+
+    return min;
+}
+
+int main() {
+    int a, b, c;
+    std::cout << "input a,b,c" << std::endl;
+
+    std::cin >> a >> b >> c;
+    std::cout << "min: " << foo(a, b, c) << std::endl;
+    return 0;
+}
+
+
+int main(){
+	int sum = 0;
+	int a = 0;
+	std::cin >> a;
+	int* arr = new int[a];
+	for(int i = 0; i < a; ++i){
+		std::cin >> arr[i];
+		sum += arr[i];
+	}
+	std::cout << "sum = " << sum << std::endl;
+	delete[] arr;
+	return 0;
+}
+
+class book{
+	public:
+		book() : title(""), author(""), year(0) {}
+
+		book(std::string title,std::string author, int year) : title(title), author(author), year(year) {}
+
+		~book() {}
+	public:
+		void setTitle(std::string title){this->title = title;}
+		void setAuthor(std::string author){this->author = author;}
+		void setYear(int year){this->year = year;}
+		std::string getTitle(){return this->title;} 
+		std::string getAuthor(){return this->author;}
+		int getYear(){return this->year;}
+		
+		void printinfo(){
+			std::cout << "Title :" << this->title << std::endl;
+			std::cout << "Author :" << this->author << std::endl;
+			std::cout << "Year :" << this->year << std::endl;
+
+		}
+	
+	private:
+		std::string title;
+		std::string author;
+		int year;
+
+};
+
+
+
+
+int main(){
+
+book b;
+b.printinfo();
+	return 0;
+}
+
+int main(){
+	int age;
+	std::string name;
+	std::cin >> name;
+	std::cin >> age;
+	std::cout << "привет меня зовут " <<  name << "мне " << age << "лет";
+
+	int a = 0;
+	int b = 0;
+	std::cin >> a;
+	std::cin >> b;
+	
+	std::cout << a + b << std::endl; 
+	
+	if (a < b){
+		std::cout << b - a << std::endl; 
+	}else{
+		std::cout << a - b << std::endl; 
+	}
+	
+	
+	std::cout << a * b  << std::endl; 
+
+	std::cout << b - a << std::endl; 
+
+	return 0;
+}
+*/
+
+void input( int& a,int& b){
+	std::cin >> a;
+	std::cin >> b;
+}
+
+int sum(const int& a, const int& b){return a + b;}
+
+void output(const int& result){std::cout << result << std::endl;}
+
+int main(){
+	int a = 0;
+	int b = 0;
+	int result = 0;
+	input(a,b); 
+	result = sum(a,b);
+	output(result);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
