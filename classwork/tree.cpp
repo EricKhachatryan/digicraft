8c3a089ecd6f45fa26db673cd8edd0f4261663d6
Eric <e1977710@gmail.com>
2025-08-20 14:05:14 +0100

    add homework tree.cpp 'search and destructor'

--- classwork/tree.cpp
+++ classwork/tree.cpp
@@ -3,6 +3,14 @@
 
 class Tree{
 struct Node;
+//Constructor/destructor
+public:
+	Tree(): root(nullptr) {}
+	~Tree(){
+		Delete(root);	
+	}	
+
+
 //public API
 public:
 	void insert(int val){
@@ -14,7 +22,15 @@ public:
 	void postOrder(){
 		showPostInner(root);
 	}
+	void inOrder(){
+		showInInner(root);
+	}
+	Node* search(int val){
+		return searchValue(root,val);
+	}
+	
 	// struct Node
+	
 private:
 	struct Node{
 	public:
@@ -26,13 +42,39 @@ private:
 // helper functions 
 private:
 	void showPreInner(Node* node){
-		if(node == nullptr){
-		} else {
-			std::cout << node->value << std::endl;
-			showPreInner(node->left);
-			showPreInner(node->right);
-		}	
+		if(node == nullptr){return;}
+		std::cout << node->value << std::endl;
+		showPreInner(node->left);
+		showPreInner(node->right);
+		
 	}	
+
+	void showPostInner(Node* node){
+		if(node == nullptr){return;}
+		showPostInner(node->left);
+		showPostInner(node->right);
+		std::cout << node->value << std::endl;
+		
+	}	
+
+
+	void showInInner(Node* node){
+		if(node == nullptr){return;}
+		showPostInner(node->left);
+		std::cout << node->value << std::endl;
+		showPostInner(node->right);
+
+	}
+	
+	Node* searchValue(Node* node,int val){
+		if( node == nullptr || node->value == val){return node;}
+		if(val < node->value){
+			return searchValue( node->left, val );
+		} else {
+			return searchValue( node->right, val );
+		}
+		
+	}
 
 	Node* insertInner(Node* node, int val){
 		if(node == nullptr){
@@ -44,7 +86,16 @@ private:
 			node->right = insertInner(node->right,val);
 		}
 		return node;
+	
 	}
+	
+	void Delete(Node* node){
+		if(node == nullptr){return;}
+		Delete(node->left);
+		Delete(node->right);
+		delete node;
+
+	}	
 //data members
 private:
 	Node* root;	
@@ -58,6 +109,16 @@ tree->insert(20);
 tree->insert(10);
 tree->insert(60);
 tree->insert(90);
-tree->preOrder();
+tree->insert(30);
+tree->insert(40);
+tree->insert(50);
+tree->insert(80);
+tree->insert(18);
+tree->insert(16);
+tree->insert(38);
+tree->insert(75);
+tree->insert(1);
+tree->postOrder();
+tree->search(50);
 return 0;
 }
