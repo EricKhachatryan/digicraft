1d7e0e00bb4983cf44e37a1fde4f92f9e7338fde
Eric <e1977710@gmail.com>
2025-08-22 14:07:08 +0100

    add homework 'Fraction.cpp'

--- classwork/fraction.cpp
+++ classwork/fraction.cpp
@@ -1,38 +1,61 @@
 #include <iostream>
 
-class Fraction(){
+class Fraction {
 public:
-	Fraction() : up(0), down(0){}
-	Fraction(int up,int down) : up(up), down(down) {
-	
-	}
-	~Fraction(){}
-public:
-	Fraction* add(const Fraction& extFract ){
-	gcd(this->down,extFract->down);
-	
-	
-	}	
+    Fraction() : num(0), denum(1) {}
 
-	int GCD(int this->down,int extFract->down){
-		int 
-	}
+    Fraction(int n, int d) {
+        if (d == 0) {
+            std::cout << "denum can't be 0 " << std::endl;
+            num = 0;
+            denum = 1;
+            return;
+        }
+        int g = gcd(n, d); 
+        num = n / g;       
+        denum = d / g;     
+        if (denum < 0) {   
+            num = -num;
+            denum = -denum;
+        }
+    }
 
+    ~Fraction() {
+    }
+
+    Fraction addConst(Fraction extFract) {
+        int newDenum = denum * extFract.denum;
+        int newNum = num * extFract.denum + denum * extFract.num;
+        int gcdValue = gcd(newNum, newDenum); 
+        return Fraction(newNum / gcdValue, newDenum / gcdValue); 
+    }
+
+    void print() const {
+        std::cout << num << "/" << denum << std::endl;
+    }
 
 private:
-	int up;
-	int down;
+    int num;
+    int denum;
 
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a < 0 ? -a : a; 
+    }
 };
 
+int main() {
+    Fraction* fr1 = new Fraction(1, 2); 
+    Fraction* fr2 = new Fraction(2, 4); 
+    Fraction result = fr1->addConst(*fr2); 
+    std::cout << "Result: ";
+    result.print(); 
+    delete fr1; 
+    delete fr2; 
+    return 0;
 
-
-
-
-
-int main(){
-Fraction* fr1 = new Fraction(1,2);
-Fraction* fr2 = new Fraction(2,4);
-fr1.add(fr2);
-return 0;
 }
