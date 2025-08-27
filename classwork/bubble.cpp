7b1105702e718826cb09f00f45358dd497954b8b
EricKhachatryan <e1977710@gmail.com>
2025-08-27 20:13:20 +0400

    add classwork 'bubble.cpp'

---
+++ classwork/bubble.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+void swap(int& a,int& b){
+	int tmp = a;
+	a = b;
+        b = tmp;	
+}
+
+void bubbleSort( std::vector<int>& arr ) {
+	bool swapped = false;
+	int size = arr.size();
+	for(int i = 0;i < size - 1;i++){
+		for(int j = 0;j < size - 1 - i;j++){
+			if(arr[j] > arr[j + 1]){
+				swap(arr[j],arr[j + 1]);
+				swapped = true;
+			}
+		}
+		if(!swapped) {break;}
+	}
+
+
+}
+
+
+int main(){
+	std::vector<int> vec = {7,2,4,3,5};
+	bubbleSort(vec);
+	for(int x : vec ){
+		std::cout << x << "";
+	}
+
+	return 0;
+}
