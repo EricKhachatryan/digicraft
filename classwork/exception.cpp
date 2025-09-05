b3a1761f6fd00cead28499d1e4f32a97454f29ea
EricKhachatryan <e1977710@gmail.com>
2025-09-05 20:40:10 +0400

    add classwork exception.cpp

--- classwork/exception.cpp
+++ classwork/exception.cpp
@@ -2,16 +2,15 @@
 #include <exception>
 
 int devaid(int a, int b){
+if( b == 0 ){
+	throw "Division by 0 Exception";
+	}
 return a/b;
 }
 int main(){
 	int a = 10;
 	int b = 0;
-
 	try{
-	if( b == 0 ){
-		throw "Division by 0 Exception";
-	}
 		std::cout << devaid(a,b) << std::endl;	
 	}catch( const char* msg){
 		std::cout << "Error " << msg << std::endl;
