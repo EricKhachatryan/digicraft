31d37d797097cac8e7f318b9b51db68af67adb3a
EricKhachatryan <e1977710@gmail.com>
2025-09-08 20:41:47 +0400

    add classwork 'exception.cpp'

--- classwork/exception.cpp
+++ classwork/exception.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <exception>
 
+/*
 int devaid(int a, int b){
 if( b == 0 ){
 	throw "Division by 0 Exception";
@@ -18,3 +19,45 @@ int main(){
 
 	return 0;	
 }
+
+*/
+
+class MyException : public std::exception {
+public: 
+	const char* what( ) const noexcept override {
+		return "My Exception";
+	}
+
+
+};
+
+void f(){
+	std::cout << "MyException" << std::endl;
+	throw new MyException();
+}
+
+void a(){
+	std::cout << "exception" << std::endl;
+	throw new std::exception();
+}
+
+int main(){
+	try{
+		f();
+		a();
+	} catch ( const std::exception* e ){
+		std::cout << "Error: " << e->what() << std::endl;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
